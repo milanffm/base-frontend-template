b668fb71ca238ea0f6eef9462a0f9b3290793376
Milan Peters <milan.peters@luka.de>
2016-04-12 16:29:37 +0200

    init commit

---
+++ bin/gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash').assign;
+
+var sass = require('gulp-sass');
+
+var DIST = './../dist/';
+
+// add custom browserify options here
+var customOpts = {
+    entries: ['./js/app.js'],
+    debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+gulp.task('js-watch', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+gulp.task('scss', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(DIST + 'css'));
+});
+
+gulp.task('scss:watch', function () {
+    gulp.watch('./scss/**/*.scss', ['scss']);
+});
+
+
+function bundle() {
+    return b.bundle()
+        // log errors if they happen
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('bundle.js'))
+        // optional, remove if you don't need to buffer file contents
+        .pipe(buffer())
+        // optional, remove if you dont want sourcemaps
+        .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+        // Add transformation tasks to the pipeline here.
+        .pipe(sourcemaps.write('./')) // writes .map file
+        .pipe(gulp.dest(DIST + 'js'));
+}
---
+++ bin/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by mpeters on 10.02.16.
+ */
+
+"use strict";
+
+console.log('new base project ddd');
