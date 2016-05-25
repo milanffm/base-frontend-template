4fa023d183eaafba4961f698126e5abbcdca6a08
Milan Peters <milan.peters@luka.de>
2016-05-25 15:38:19 +0200

    add polyfills

---
+++ src/js/polyfills.js
@@ -0,0 +1,17 @@
+/**
+ * Created by milan on 09.06.15.
+ */
+'use strict';
+
+(function() {
+
+    //require neede polyfill
+
+	// require('./node_core/polyfills/Console')();
+	// require('./node_core/polyfills/QuerySelector')();
+	// require('./node_core/polyfills/es5shim')();
+	// require('./node_core/polyfills/es5sham')();
+	// require('./node_core/polyfills/Raf')();
+	// require('./node_core/polyfills/ClassList')();
+
+})();
